d2f2a69e52e087797b555c6a653aafc0cbff5c20
Gudiess <ngqwala@gmail.com>
2021-05-31 09:50:54 +0200

    My Report
    Learning

---
+++ Controllers/StudentCoerceDatesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Report.Models;
+
+namespace Report.Controllers
+{
+    public class StudentCoerceDatesController : Controller
+    {
+        private ConnectionString db = new ConnectionString();
+
+        // GET: StudentCoerceDates
+        public ActionResult Index()
+        {
+            var studentCoerceDates = db.StudentCoerceDates.Include(s => s.UserDetail);
+            return View(studentCoerceDates.ToList());
+        }
+
+        // GET: StudentCoerceDates/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentCoerceDate studentCoerceDate = db.StudentCoerceDates.Find(id);
+            if (studentCoerceDate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentCoerceDate);
+        }
+
+        // GET: StudentCoerceDates/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.UserDetails, "UserId", "Name");
+            return View();
+        }
+
+        // POST: StudentCoerceDates/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SCourseDateId,UserId,StartDate,PlanedFinishDate,CourseId")] StudentCoerceDate studentCoerceDate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StudentCoerceDates.Add(studentCoerceDate);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.UserDetails, "UserId", "Name", studentCoerceDate.UserId);
+            return View(studentCoerceDate);
+        }
+
+        // GET: StudentCoerceDates/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentCoerceDate studentCoerceDate = db.StudentCoerceDates.Find(id);
+            if (studentCoerceDate == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.UserDetails, "UserId", "Name", studentCoerceDate.UserId);
+            return View(studentCoerceDate);
+        }
+
+        // POST: StudentCoerceDates/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SCourseDateId,UserId,StartDate,PlanedFinishDate,CourseId")] StudentCoerceDate studentCoerceDate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentCoerceDate).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.UserDetails, "UserId", "Name", studentCoerceDate.UserId);
+            return View(studentCoerceDate);
+        }
+
+        // GET: StudentCoerceDates/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentCoerceDate studentCoerceDate = db.StudentCoerceDates.Find(id);
+            if (studentCoerceDate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentCoerceDate);
+        }
+
+        // POST: StudentCoerceDates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StudentCoerceDate studentCoerceDate = db.StudentCoerceDates.Find(id);
+            db.StudentCoerceDates.Remove(studentCoerceDate);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/UserDetailsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Report.Models;
+
+namespace Report.Controllers
+{
+    public class UserDetailsController : Controller
+    {
+        private ConnectionString db = new ConnectionString();
+
+        // GET: UserDetails
+        public ActionResult Index()
+        {
+            var userDetails = db.UserDetails.Include(u => u.UserLogin);
+            return View(userDetails.ToList());
+        }
+
+        // GET: UserDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDetail userDetail = db.UserDetails.Find(id);
+            if (userDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userDetail);
+        }
+
+        // GET: UserDetails/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserLogID = new SelectList(db.UserLogins, "UserLogID", "email");
+            return View();
+        }
+
+        // POST: UserDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,Name,Surname,ContactNum,Id,Gender,Type,UserLogID")] UserDetail userDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserDetails.Add(userDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserLogID = new SelectList(db.UserLogins, "UserLogID", "email", userDetail.UserLogID);
+            return View(userDetail);
+        }
+
+        // GET: UserDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDetail userDetail = db.UserDetails.Find(id);
+            if (userDetail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserLogID = new SelectList(db.UserLogins, "UserLogID", "email", userDetail.UserLogID);
+            return View(userDetail);
+        }
+
+        // POST: UserDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,Name,Surname,ContactNum,Id,Gender,Type,UserLogID")] UserDetail userDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserLogID = new SelectList(db.UserLogins, "UserLogID", "email", userDetail.UserLogID);
+            return View(userDetail);
+        }
+
+        // GET: UserDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDetail userDetail = db.UserDetails.Find(id);
+            if (userDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userDetail);
+        }
+
+        // POST: UserDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserDetail userDetail = db.UserDetails.Find(id);
+            db.UserDetails.Remove(userDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
